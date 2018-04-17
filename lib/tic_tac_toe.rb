f535cfe7a435dc1092feef5ae4473aa86ea770c9
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-17 01:03:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,8 +55,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-display_board(board)
   
 def input_to_index(user_input)
   converted_input = user_input.to_i-1
